f91c53cd6634f537e06c198ebe5d3e295edff3e0
jeremiahdingal <jeremiah@gad.ai>
2020-10-16 21:22:10 +0100

    minor adjustments

--- backend/middleware/authorization.js
+++ backend/middleware/authorization.js
@@ -5,12 +5,14 @@ var createError = require('http-errors');
 let checkToken = (req, res, next) => {
 
     let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-    if (token?.startsWith('Bearer ')) {
-        // Remove Bearer from string
-        token = token.slice(7, token.length);
-    }
+    
 
     if (token) {
+        if (token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length);
+        }
+        
         if (token===process.env.SECRET){
             next();
         } else {
